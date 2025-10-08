8c760863c5bf350136c248728c416f05b51b5d25
smit4k <smit.patil.usa@gmail.com>
2025-10-08 14:32:36 -0400

    Remove pointer cursor on profile picture hover

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -5,7 +5,7 @@ const ProfilePicture: React.FC = () => {
     <img
       src="/smit_pfp.svg"
       alt="Smit's profile picture"
-      className="w-10 h-10 rounded-full object-cover border-2 border-border profile-spin hover:cursor-pointer"
+      className="w-10 h-10 rounded-full object-cover border-2 border-border profile-spin"
     />
   );
 };
