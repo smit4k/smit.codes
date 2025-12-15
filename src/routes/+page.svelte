ac77a0317d80495f3d6dfc88dcbef61ae8706111
smit4k <smit@smit.codes>
2025-12-14 22:42:52 -0500

    Update description and contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,13 +12,14 @@
 		Hey! I’m Smit, welcome to my homepage,
 		<a href="https://smit.codes">smit.codes</a>!
 		<br />
-		I'm currently a high school student in Michigan, interested in sftware development and UI/UX design.
+		I'm currently a high school student in Michigan, interested in software development, Linux, and UI/UX
+		design.
 	</p>
 
 	<h2>Contact</h2>
 	<hr />
 
-	<p>If you have any questions or comments, you can contact me through:</p>
+	<p>If you have any questions or comments, you can contact me through the following ways:</p>
 	<ul>
 		<li>Email: <a href="mailto:smit@smit.codes">smit@smit.codes</a></li>
 		<li>Discord: sm.it</li>
