f27f9bc3798e726eb7e3fedb288de83692bbb4d0
Smit Patil <smit@smit.codes>
2025-12-15 14:00:12 -0500

    Add barebones Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,17 +1,41 @@
 <script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Container from '$lib/components/Container.svelte';
 </script>
 
-<div class="container">
-	<p>Projects are a work in progress</p>
-</div>
+<Container>
+	<Navbar />
+	<h1>Projects</h1>
+	<p>
+		You can find all of my projects on my Github, <a href="https://github.com/smit4k">@smit4k</a>.
+		Some of my most interesting or complete projects will be put here, with articles explaining them
+		in detail.
+	</p>
+	<hr />
+</Container>
 
 <style>
 	:global(body) {
 		background-color: black;
 	}
 
+	h1 {
+		margin-top: 0.5rem;
+		margin-bottom: 0.3rem;
+		color: white;
+	}
+
 	p {
 		font-family: Arial;
-		color: white;
+		color: #ddd;
+	}
+
+	hr {
+		margin: 0.3rem 0;
+		border-color: #444;
+	}
+
+	a {
+		color: #58a6ff;
 	}
 </style>
