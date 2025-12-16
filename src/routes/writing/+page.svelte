f9b0994dbc233ad8dacd979a0e1fdfcdc324e80c
Smit Patil <smit@smit.codes>
2025-12-15 21:49:37 -0500

    Add barebones Writing page

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,17 +1,33 @@
 <script>
+	import Container from '$lib/components/Container.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="container">
-	<p>Writing is a work in progress</p>
-</div>
+<Container>
+	<Navbar />
+	<h1>Writing</h1>
+	<p>
+		Here are some topics I've written about! These posts cover a variety of technology related
+		topics, so feel free to take a look and see if you find anything interesting!
+	</p>
+	<hr />
+	<Footer />
+</Container>
 
 <style>
 	:global(body) {
 		background-color: black;
 	}
 
+	h1 {
+		margin-top: 0.5rem;
+		margin-bottom: 0.3rem;
+		color: white;
+	}
+
 	p {
 		font-family: Arial;
-		color: white;
+		color: #ddd;
 	}
 </style>
