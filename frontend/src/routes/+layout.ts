c7d125dbd69f192ebdf23126bea4a1447fcbc982
Smit Patil <smit@smit.codes>
2025-12-22 23:46:10 -0500

    Remove vercel speed insights and analytics

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,6 +1,2 @@
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 import { dev } from '$app/environment';
-import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
-injectAnalytics({ mode: dev ? 'development' : 'production' });
-injectSpeedInsights();
