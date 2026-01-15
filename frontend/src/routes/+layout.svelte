d07de55bfb71a014e6746f0f07a1ac1c87e512d9
Smit Patil <smit@smit.codes>
2026-01-14 23:51:51 -0500

    Add svelte-top-loader loading bar to entire site

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.ico';
 	import '$lib/homepage.css';
+	import { SvelteKitTopLoader } from 'sveltekit-top-loader';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<SvelteKitTopLoader color="#58a6ff" showSpinner={false} />
 
 {@render children()}
