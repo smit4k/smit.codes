0494092c5999f290b9b8d913cada02801b778897
Smit Patil <smit@smit.codes>
2026-02-19 00:26:58 -0500

    Add Callout svelte component
    A svelte component of the callout I use in markdown

---
+++ frontend/src/lib/components/Callout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let type: 'info' | 'warning' | 'note' | 'danger' = 'info';
+	export let title: string = type.charAt(0).toUpperCase() + type.slice(1);
+</script>
+
+<div class="callout callout-{type}">
+	<div class="callout-header">
+		<span class="callout-icon"></span>
+		<span class="callout-title">{title}</span>
+	</div>
+	<div class="callout-body">
+		<slot />
+	</div>
+</div>
